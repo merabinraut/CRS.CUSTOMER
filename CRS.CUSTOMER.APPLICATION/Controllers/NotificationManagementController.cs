edf80d5073593e436d363f0c5c0c797939ffd122
itsrabinraut <rabin.raut@globalsquareit.com>
2024-02-05 16:53:11 +0545

    notification updates

--- CRS.CUSTOMER.APPLICATION/Controllers/NotificationManagementController.cs
+++ CRS.CUSTOMER.APPLICATION/Controllers/NotificationManagementController.cs
@@ -1,8 +1,11 @@
 ﻿using CRS.CUSTOMER.APPLICATION.Library;
 using CRS.CUSTOMER.APPLICATION.Models.NotificationManagement;
 using CRS.CUSTOMER.BUSINESS.NotificationManagement;
+using CRS.CUSTOMER.SHARED;
 using CRS.CUSTOMER.SHARED.NotificationManagement;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace CRS.CUSTOMER.APPLICATION.Controllers
@@ -24,12 +27,46 @@ namespace CRS.CUSTOMER.APPLICATION.Controllers
             var responseModel = new List<NotificationDetailModel>();
             responseModel = dbResponse.MapObjects<NotificationDetailModel>();
             ViewBag.ActionPageName = "NavMenu";
-            //if (responseModel.Count > 0)
-            //    ViewBag.PageTitle = $"{Resources.Resource.Notifications} ({responseModel.Count})";
-            //else
-            ViewBag.PageTitle = Resources.Resource.Notifications;
-
+            if (responseModel?.FirstOrDefault()?.UnReadNotification != null &&
+                    int.TryParse(responseModel.FirstOrDefault().UnReadNotification, out int unReadCount) &&
+                    unReadCount > 0)
+                ViewBag.PageTitle = $"{Resources.Resource.Notifications} ({responseModel.FirstOrDefault().UnReadNotification})";
+            else
+                ViewBag.PageTitle = Resources.Resource.Notifications;
             return View(responseModel);
+        }
+
+        [HttpPost, OverrideActionFilters]
+        public JsonResult HasUnReadNotification()
+        {
+            var CustomerId = ApplicationUtilities.GetSessionValue("AgentId").ToString();
+            CustomerId = !string.IsNullOrEmpty(CustomerId) ? CustomerId.DecryptParameter() : null;
+            if (!string.IsNullOrEmpty(CustomerId))
+            {
+                var dbResponse = _buss.HasUnReadNotification(CustomerId);
+                if (dbResponse)
+                {
+                    return Json(new { HasUnReadNotification = true });
+                }
+            }
+            return Json(new { HasUnReadNotification = false });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken, OverrideActionFilters]
+        public JsonResult ManageNotificationReadStatus()
+        {
+            var dbRequest = new Common()
+            {
+                AgentId = !string.IsNullOrEmpty(ApplicationUtilities.GetSessionValue("AgentId").ToString()) ? ApplicationUtilities.GetSessionValue("AgentId").ToString().DecryptParameter() : null,
+                ActionUser = ApplicationUtilities.GetSessionValue("Username").ToString()
+            };
+            if (!string.IsNullOrEmpty(dbRequest.AgentId) && !string.IsNullOrEmpty(dbRequest.ActionUser))
+            {
+                var dbResponse = _buss.ManageNotificationReadStatus(dbRequest);
+                if (dbResponse != null && dbResponse.Code == ResponseCode.Success) return Json(new { Code = "0", Message = dbResponse.Message ?? "Success", PageTitle = Resources.Resource.Notifications });
+                return Json(new { Code = "1", Message = dbResponse.Message ?? "Invalid request" });
+            }
+            return Json(new { Code = "1", Message = "Something went wrong. Please try again later." });
         }
     }
 }
