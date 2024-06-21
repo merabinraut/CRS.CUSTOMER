ae7f255003ceda5eeb5b7e280d45fdf5e0a9c313
Sandesh Bhusal <sandeshbhusal417@gmail.com>
2024-06-21 17:10:28 +0545

    fix: Button is not clickable. Only Text inside button is clickable

--- CRS.CUSTOMER.APPLICATION/Content/assets/js/modal.js
+++ CRS.CUSTOMER.APPLICATION/Content/assets/js/modal.js
@@ -2,26 +2,26 @@
 
 // Function to open a specific modal
 function openModal(modalId) {
-  var modal = document.getElementById(modalId);
-  if (modal) {
-    modal.style.display = "flex";
-  }
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "flex";
+    }
 }
 
 // Function to close a specific modal
 function closeModal(modalId) {
-  var modal = document.getElementById(modalId);
-  if (modal) {
-    modal.style.display = "none";
-  }
+    var modal = document.getElementById(modalId);
+    if (modal) {
+        modal.style.display = "none";
+    }
 }
 
 // Close the modal if the overlay is clicked
 window.onclick = function (event) {
-  var modals = document.getElementsByClassName("modal");
-  for (var i = 0; i < modals.length; i++) {
-    if (event.target == modals[i]) {
-      closeModal(modals[i].id);
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target == modals[i]) {
+            closeModal(modals[i].id);
+        }
     }
-  }
 };
