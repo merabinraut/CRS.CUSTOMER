a479edac9dc8a63b5d787f6922a04dedab341a16
imrabinraut <imrabinraut@gmail.com>
2024-06-22 16:54:35 +0545

    updates

--- CRS.CUSTOMER.REPOSITORY/LocationManagement/LocationManagementRepository.cs
+++ CRS.CUSTOMER.REPOSITORY/LocationManagement/LocationManagementRepository.cs
@@ -12,10 +12,13 @@ namespace CRS.CUSTOMER.REPOSITORY.LocationManagement
         private readonly RepositoryDao _dao;
         public LocationManagementRepository(RepositoryDao dao) => _dao = dao;
 
-        public ClubDetailCommon GetClubDetailById(string clubId, string CustomerId = "")
+        public ClubDetailCommon GetClubDetailById(string clubId, string CustomerId = "", string clubcode = "")
         {
             string sql = "sproc_customer_club_and_host_management @Flag='cd'";
-            sql += " ,@ClubId=" + _dao.FilterString(clubId);
+            if (string.IsNullOrEmpty(clubcode))
+                sql += " ,@ClubId=" + _dao.FilterString(clubId);
+            else
+                sql += " ,@ClubCode=" + _dao.FilterString(clubcode);
             sql += " ,@customerAgentId=" + _dao.FilterString(CustomerId);
             var dbResp = _dao.ExecuteDataTable(sql);
             if (dbResp != null && dbResp.Rows.Count == 1)
