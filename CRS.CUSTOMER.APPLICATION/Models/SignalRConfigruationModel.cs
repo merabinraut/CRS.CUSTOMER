9e2d2b676234a957a61013708c23b812689207b0
itsrabinraut <rabin.raut@globalsquareit.com>
2024-09-25 17:47:16 +0545

    api integrating portion

---
+++ CRS.CUSTOMER.APPLICATION/Models/SignalRConfigruationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CRS.CUSTOMER.APPLICATION.Models
+{
+    public class SignalRConfigruationModel
+    {
+        public string baseURL { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public string securityKey { get; set; }
+        public string securityIV { get; set; }
+    }
+}
