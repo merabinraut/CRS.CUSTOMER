7a8e95ba373eeff058c9fea905c325ff4e43d6ef
thejanak <janak.mahato@globalsquareit.com>
2024-10-15 15:50:30 +0545

    updated main page

--- CRS.CUSTOMER.APPLICATION/Models/LocationManagementV2/LocationManagementModel.cs
+++ CRS.CUSTOMER.APPLICATION/Models/LocationManagementV2/LocationManagementModel.cs
@@ -44,6 +44,8 @@ namespace CRS.CUSTOMER.APPLICATION.Models.LocationManagementV2
         public string IsBookmarked { get; set; }
         public int TotalComment { get; set; }
         public int AverageRating { get; set; }
+        public string ClubCategory { get; set; }
+        public string LandLine { get; set; }
         public List<string> ClubGalleryImage { get; set; } = new List<string>();
         public List<string> HostGalleryImage { get; set; } = new List<string>();
         public List<ClubWeeklyScheduleModel> ClubWeeklyScheduleList { get; set; } = new List<ClubWeeklyScheduleModel>();
