fb7d0ae16e7e1c745702b9f8ce4a1318df0534dd
nabinshrestha024 <nabin.shrestha@globalsquareit.com>
2024-10-21 17:58:58 +0545

    fix: button font weight

--- CRS.CUSTOMER.APPLICATION/Content/assets/js/call_popup_modal.js
+++ CRS.CUSTOMER.APPLICATION/Content/assets/js/call_popup_modal.js
@@ -36,7 +36,7 @@ function callPopupModal(clubName, clubLogo, landlineNumber, locationName, type) 
                                         <p>電話でのご予約はポイント</p>
                                         <p>対象外となりますのでご注意ください。</p>
                                     </div>
-                                    <div style="padding-bottom:28px"><a style="text-align:center; line-height:130%; color:#888" class="text-[#888] text-[12px] font-bold flex justify-center">ネット予約でポイントを獲得する</a></div>
+                                    <div style="padding-bottom:28px"><a style="text-align:center; line-height:130%; color:#888" class="text-[#888] text-[12px] font-[600] flex justify-center">ネット予約でポイントを獲得する</a></div>
                                 </div>
                             </div>
                         </div>
@@ -76,7 +76,7 @@ function callPopupModal(clubName, clubLogo, landlineNumber, locationName, type) 
                                     <span>電話でのご予約はポイント</span>
                                     <span>対象外となりますのでご注意ください。</span>
                                 </div>
-                                <div class="" style="padding-bottom:28px"><a style="text-align:center;line-height:130%" class="text-[#D75A8B] text-[12px] underline font-bold flex justify-center" href="#">ネット予約でポイントを獲得する</a></div>
+                                <div class="" style="padding-bottom:28px"><a style="text-align:center;line-height:130%" class="text-[#D75A8B] text-[12px] underline font-[600] flex justify-center" href="#">ネット予約でポイントを獲得する</a></div>
                             </div>
                         </div>
                     </div>
